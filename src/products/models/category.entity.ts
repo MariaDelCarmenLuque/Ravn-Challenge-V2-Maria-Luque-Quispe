85adaf5c8280f26aed632db99dffc9e686262fc8
MariaDelCarmenLuque <maritcarmn@gmail.com>
2022-03-12 15:11:24 -0300

    Created Category entity

---
+++ src/products/models/category.entity.ts
@@ -0,0 +1,18 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('categories')
+export class Category {
+    @PrimaryGeneratedColumn({
+        unsigned: true,
+        type: 'smallint',
+    })
+    id: number;
+
+    @Column({
+        name: 'name',
+        type: 'character varying',
+        length: 100,
+        nullable: false,
+    })
+    name: string;
+}
