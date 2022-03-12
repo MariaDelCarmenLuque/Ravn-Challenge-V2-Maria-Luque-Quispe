0916b2749d97c4a09b1f24d481fd61bee9404df2
MariaDelCarmenLuque <maritcarmn@gmail.com>
2022-03-12 09:19:29 -0300

    Added Create-Product DTO

---
+++ src/products/models/create-product.dto.ts
@@ -0,0 +1,24 @@
+import { IsNumber, IsString, Length, Max, Min } from "class-validator"
+
+export class CreateProductDto {
+    @IsString()
+    @Length(1,100)
+    name: string;
+
+    @IsString()
+    @Length(1,1000)
+    description: string;
+
+    @IsNumber()
+    @Min(1)
+    @Max(1000000)
+    stock: number;
+    
+    @IsNumber()
+    @Min(0)
+    @Max(1000000)
+    price: number;
+
+   //CategoryId
+   
+}
