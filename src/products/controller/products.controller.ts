b6602f31863657029d0f845952e4644a3a573142
MariaDelCarmenLuque <maritcarmn@gmail.com>
2022-03-12 02:34:07 -0300

    Added endpoints in product controller

--- src/products/controller/products.controller.ts
+++ src/products/controller/products.controller.ts
@@ -1,4 +1,38 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post} from '@nestjs/common';
+import { Product } from '../models/product.entity';
+import { ProductsService } from '../service/products.service';
 
 @Controller('products')
-export class ProductsController {}
+export class ProductsController {
+    constructor(
+        private readonly productsService: ProductsService,
+    ){}
+
+
+    @Get()
+    async findAll():Promise<Product[]>  {
+    
+        return this.productsService.getAll();
+    }
+
+    @Get(':id')
+    async findOne(@Param('id')id:number): Promise<Product> {
+    
+        return await this.productsService.findOne(id);
+    }
+    @Post()
+    async create(@Body() createProductDTO:CreateProductDto): Promise<Product>{
+        return await this.productsService.create(createProductDTO);
+    }
+
+    @Patch(':id')
+    async update(@Param('id') id: number,@Body() editProductDTO: EditProductDto): Promise<void>{
+        return await this.productsService.update(id,editProductDTO);
+    }
+
+    @Delete(':id')
+    async delete(@Param('id') id:number){
+        return this.productsService.delete(id);
+    }
+
+}
