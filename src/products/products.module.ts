2c15cf13bc963afca9223f94e9dd515a3f075b01
MariaDelCarmenLuque <maritcarmn@gmail.com>
2022-03-12 11:15:53 -0300

    Added imports and exports in product module

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductsController } from './controller/products.controller';
+import { Product } from './models/product.entity';
 import { ProductsService } from './service/products.service';
 
 @Module({
   controllers: [ProductsController],
-  providers: [ProductsService]
+  providers: [ProductsService],
+  imports: [
+    TypeOrmModule.forFeature([Product]),
+  ],
+  exports: [ProductsService],
 })
 export class ProductsModule {}
