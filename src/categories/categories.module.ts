758a545597c7590c6b0e76263d0bdf4f54de6a71
MariaDelCarmenLuque <maritcarmn@gmail.com>
2022-03-12 22:08:14 -0300

    Added imports and exports in categories.module file

--- src/categories/categories.module.ts
+++ src/categories/categories.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
-import { CategoriesController } from './categories.controller';
-import { CategoriesService } from './categories.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Category } from './category.entity';
+import { CategoriesController } from './controller/categories.controller';
+import { CategoriesService } from './service/categories.service';
 
 @Module({
   controllers: [CategoriesController],
-  providers: [CategoriesService]
+  providers: [CategoriesService],
+  imports: [
+    TypeOrmModule.forFeature([Category]),
+  ],
+  exports: [CategoriesService],
 })
 export class CategoriesModule {}
