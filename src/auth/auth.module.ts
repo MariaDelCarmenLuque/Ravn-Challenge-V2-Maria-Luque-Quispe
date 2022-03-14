ec840e7d634541275e85a4ea8d348ea06521a544
MariaDelCarmenLuque <maritcarmn@gmail.com>
2022-03-14 10:06:41 -0300

    Added Auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,13 +1,35 @@
-import { Module } from '@nestjs/common';
-import { UsersModule } from 'src/users/users.module';
-import { AuthController } from './auth.controller';
-import { AuthService } from './auth.service';
+import { Module } from "@nestjs/common";
+import { UsersModule } from "src/users/users.module";
 import { PassportModule } from '@nestjs/passport';
-import { LocalStrategy } from './strategies/local.strategy';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './constants';
+import { AuthService } from "./auth.service";
+import { AuthController } from "./auth.controller";
+import { LocalStrategy } from "./strategies/local.strategy";
+import { JwtStrategy } from "./strategies/jwt.strategy";
+import { JwtAuthGuard } from "./guards/jwt-auth.guard";
+import { RolesGuard } from "./guards/roles.guard";
+
 
 @Module({
-  controllers: [AuthController],
-  providers: [AuthService, LocalStrategy],
-  imports: [UsersModule, PassportModule]
+  imports: [
+    UsersModule, 
+    PassportModule,
+    JwtModule.register({
+    secret: jwtConstants.secret,
+  }),
+  ],
+  providers: [AuthService, LocalStrategy, JwtStrategy,
+    {
+      provide: 'APP_GUARD',
+      useClass: JwtAuthGuard,
+    },
+    {
+      provide: 'APP_GUARD',
+      useClass: RolesGuard,
+    },
+  ],
+  controllers: [AuthController]
+  
 })
 export class AuthModule {}
