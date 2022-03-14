a0f2f72e41b94ea08b6bd94040e544fd7bc865b7
MariaDelCarmenLuque <maritcarmn@gmail.com>
2022-03-14 02:07:05 -0300

    Added localStrategy in providers and PasssportModule in imports

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { UsersModule } from 'src/users/users.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { PassportModule } from '@nestjs/passport';
+import { LocalStrategy } from './strategies/local.strategy';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, LocalStrategy],
+  imports: [UsersModule, PassportModule]
 })
 export class AuthModule {}
