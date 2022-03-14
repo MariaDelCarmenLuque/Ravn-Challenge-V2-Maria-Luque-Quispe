bc1820a4615606e7f054f59b3cdc98e403203229
MariaDelCarmenLuque <maritcarmn@gmail.com>
2022-03-14 15:46:09 -0300

    Added config in main.ts for api documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
+import {
+  SwaggerModule,
+  SwaggerDocumentOptions,
+  DocumentBuilder,
+} from '@nestjs/swagger';
+
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+  .setTitle('Dessert Store API')
+  .setDescription('')
+  .setVersion('1.0')
+  .addBearerAuth()
+  .build();
+
+  const options: SwaggerDocumentOptions = {
+    operationIdFactory: (controllerKey: string, methodKey: string) => methodKey,
+  };
+  const document = SwaggerModule.createDocument(app, config, options);
+  SwaggerModule.setup('api/docs', app, document);
+
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
