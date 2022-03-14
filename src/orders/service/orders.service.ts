caa1a93c80338fb4344a5360affa07e8cd3e6bf8
MariaDelCarmenLuque <maritcarmn@gmail.com>
2022-03-14 14:28:50 -0300

    Added orders service

--- src/orders/service/orders.service.ts
+++ src/orders/service/orders.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Product } from 'src/products/models/product.entity';
+import { Repository } from 'typeorm';
+import { Order } from '../models/orders.entity';
 
 @Injectable()
-export class OrdersService {}
+export class OrdersService {
+    constructor(
+        @InjectRepository(Order)
+        private readonly orderRepository: Repository<Order>,
+        @InjectRepository(Product)
+        private readonly products: Repository<Product>,
+    ) {}
+
+    findAllFromCart(cartId: number): Promise<Order[]> {
+        return this.orderRepository.find({
+          cartId: cartId,
+        });
+      }
+}
