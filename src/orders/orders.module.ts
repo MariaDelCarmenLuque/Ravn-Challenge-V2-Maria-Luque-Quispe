f2d96ee3c3d793adf1b1853bcdfdce86bf60d33e
MariaDelCarmenLuque <maritcarmn@gmail.com>
2022-03-14 14:29:26 -0300

    Updated orders module

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
-import { OrdersService } from './service/orders/orders.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Product } from 'src/products/models/product.entity';
+import { OrdersController } from './controller/orders.controller';
+import { Order } from './models/orders.entity';
+import { OrdersService } from './service/orders.service';
 
 @Module({
-  providers: [OrdersService]
+  providers: [OrdersService],
+  controllers:[OrdersController],
+  imports: [TypeOrmModule.forFeature([Order, Product])]
 })
 export class OrdersModule {}
