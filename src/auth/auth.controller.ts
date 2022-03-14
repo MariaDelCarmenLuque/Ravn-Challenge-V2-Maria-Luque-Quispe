9278559bdad6e62a348799ed63d20289bbcaa4ed
MariaDelCarmenLuque <maritcarmn@gmail.com>
2022-03-14 16:52:41 -0300

    Added Public() and UseGuards() decorators in Auth  endpoints

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,23 +1,38 @@
-import { Body, Controller, Post, Req } from '@nestjs/common';
+import { Body, Controller, Post, Req, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
+import {
+    ApiTags,
+    ApiCreatedResponse,
+    ApiBadRequestResponse,
+    ApiBearerAuth,
+    ApiOperation
+  } from '@nestjs/swagger';
+  
 import { CreateUserDto } from 'src/users/models/create-user.dto';
 import { LogInUserDto } from 'src/users/models/login-user.dto';
 import { AuthService } from './auth.service';
+import { Public } from './decorators/public.decorator';
+import { LocalAuthGuard } from './guards/local-auth.guard';
 
+@ApiBearerAuth()
+@ApiTags('Authentication')
 @Controller('auth')
 export class AuthController {
     constructor(private authService: AuthService){}
-@Post('register')
-async register(@Body() user: CreateUserDto) {
-    await this.authService.checkEmail(user);
+   
+    @Public()
+    @Post('signUp')
+    async register(@Body() user: CreateUserDto) {
+        await this.authService.checkEmail(user);
 
-    await this.authService.create(user);
-}
-  
-
-@Post('login')
-async login(@Body() user: LogInUserDto, @Req() req) {
-    return this.authService.login(req.user, user.password);
-  }
-
+        await this.authService.create(user);
+    }
     
+    @UseGuards(LocalAuthGuard)
+    @Public()
+    @Post('signIn')
+    async login(@Body() user: LogInUserDto, @Req() req) {
+        return this.authService.login(req.user, user.password);
+    }
+
+        
 }
