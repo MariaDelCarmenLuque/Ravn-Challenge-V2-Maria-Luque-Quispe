c7550a6eb627cb3b42c3afcc0eb397a6f7ca6fe5
MariaDelCarmenLuque <maritcarmn@gmail.com>
2022-03-14 02:09:29 -0300

    Added validatePassword and login methods in Auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,15 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+import { User } from 'src/users/models/user.entity';
 import { UsersService } from 'src/users/service/users.service';
 
 @Injectable()
 export class AuthService {
-    constructor(private usersService: UsersService){}
+    constructor(
+        private usersService: UsersService,
+        private jwtService: JwtService
+        ){}
 
     // validateUser() method for of retrieving a user and verifying the password
     async validateUser(email: string): Promise<any> {
@@ -22,4 +28,22 @@ export class AuthService {
         return userData;
 
       }
+    async validatePassword(dbPassword, password): Promise<any> {
+        const passwordMatch = await bcrypt.compare(password, dbPassword);
+    
+        if (!passwordMatch) {
+          throw new HttpException('WRONG_PASSWORD', HttpStatus.FORBIDDEN);
+        }
+    }
+    
+
+    async login(user: User, receivedPassword:string) {
+        const dbPassword = await this.usersService.findPassword(user.id);
+
+        await this.validatePassword(dbPassword, receivedPassword);
+
+        return {
+            access_token: this.jwtService.sign({...user}),
+        };
+    }
 }
