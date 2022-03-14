1db30960212f2d2a855b27d9fd4ac6ff109e5d3d
MariaDelCarmenLuque <maritcarmn@gmail.com>
2022-03-14 09:53:51 -0300

    Added auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,18 @@
 import { Body, Controller, Post, Req } from '@nestjs/common';
+import { CreateUserDto } from 'src/users/models/create-user.dto';
 import { LogInUserDto } from 'src/users/models/login-user.dto';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
     constructor(private authService: AuthService){}
+@Post('register')
+async register(@Body() user: CreateUserDto) {
+    await this.authService.checkEmail(user);
+
+    await this.authService.create(user);
+}
+  
 
 @Post('login')
 async login(@Body() user: LogInUserDto, @Req() req) {
