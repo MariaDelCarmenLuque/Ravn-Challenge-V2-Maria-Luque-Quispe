e01f1c9441a7fc9797a84b6712d79382dd1829e3
MariaDelCarmenLuque <maritcarmn@gmail.com>
2022-03-14 22:33:39 -0300

    Updated GET() endpoint in controller and service

--- src/cart/controller/cart.controller.ts
+++ src/cart/controller/cart.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get} from '@nestjs/common';
+import { Cart } from '../entity/cart.entity';
+import { CartService } from '../service/cart.service';
 
-@Controller('cart')
-export class CartController {}
+@Controller('carts')
+export class CartController {
+    constructor(
+        private readonly cartService: CartService
+    ) {}
+    
+   
+    @Get('')
+    async getAll(): Promise<Cart[]> {
+       return this.cartService.findAll();
+    }
+}
--- src/cart/service/cart.service.ts
+++ src/cart/service/cart.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Cart } from '../entity/cart.entity';
 
 @Injectable()
-export class CartService {}
+export class CartService {
+  constructor(
+    @InjectRepository(Cart)
+    private readonly cartRepository: Repository<Cart>){}
+
+    async findAll(): Promise<Cart[]> {
+      
+        return this.cartRepository.find();
+      }
+}
