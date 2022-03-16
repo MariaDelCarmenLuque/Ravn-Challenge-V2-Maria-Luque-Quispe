8056dde9eb11a8eb422070024370ce2f82c244c2
MariaDelCarmenLuque <maritcarmn@gmail.com>
2022-03-16 01:44:00 -0300

    Fixed create method

--- src/orders/service/orders.service.ts
+++ src/orders/service/orders.service.ts
@@ -12,11 +12,13 @@ export class OrdersService {
     private readonly orderRepository: Repository<Order>,
     @InjectRepository(Product)
     private readonly products: Repository<Product>,
+    @InjectRepository(Cart)
+    private readonly cartsRepository: Repository<Cart>,
 ) {}
 
   async findAllFromCart(cartId: number): Promise<Order[]> {
       return await this.orderRepository.find({
-        cartId: cartId,
+        where:{cartId: cartId},
       });
   }
 
@@ -32,10 +34,10 @@ export class OrdersService {
     if (!product.isAvailable(body.quantity)) {
       throw new ForbiddenException('Required quantity not available');
     }
-    const price=product.price;
     const subtotal = product.getFinalPrice(body.quantity);
-
-    return this.orderRepository.save(this.orderRepository.merge({ ...body,price, subtotal }, { cartId, productId }));
+    body.subtotal=subtotal,
+    this.orderRepository.merge({ ...body, subtotal }, { cartId, productId })
+    return this.orderRepository.save(body);
   }
 
 
